51c1a919547921bfe9536b83bdd272cc9b84ac7c
iandunn <iandunn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-06 17:05:27 +0000

    Change wp-admin page headers to `h1` for accessibility.
    See https://make.wordpress.org/plugins/2015/08/03/4-3-change-to-plugin-dashboard-pages/
    
    
    git-svn-id: https://plugins.svn.wordpress.org/rescue-children-banner/trunk@1214241 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/settings.php
+++ views/settings.php
@@ -1,6 +1,6 @@
 <div class="wrap">
 	<div id="icon-options-general" class="icon32"><br /></div>
-	<h2><?php _e( 'Rescue Children Banner Settings', 'rescue-children-banner' ); ?></h2> 
+	<h1><?php _e( 'Rescue Children Banner Settings', 'rescue-children-banner' ); ?></h1>
 
 	<form method="post" action="<?php echo esc_attr( esc_url( admin_url( 'options.php' ) ) ); ?>">
 		<?php settings_fields( self::SETTINGS_PAGE ); ?>
